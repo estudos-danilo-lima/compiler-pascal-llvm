d0e781552d81127028f087d1606ce4c433e6418d
Fernando Azevedo <fernando.azevedo.peres@gmail.com>
2022-08-12 18:31:23 -0300

    Tests 	- Teste das operações aritméticas

---
+++ tests/exp/var_arit.pas
@@ -0,0 +1,13 @@
+program exemplo;
+var
+    num: integer;
+begin
+    num := 4 + 2;
+    Writeln('Result ', '4 + 2: ', num);
+    num := 4 - 2;
+    Writeln('Result ', '4 - 2: ', num);
+    num := 4 * 2;
+    Writeln('Result ', '4 * 2: ', num);
+    num := 4 / 2;
+    Writeln('Result ', '4 / 2: ', num);
+end.
